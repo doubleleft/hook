e778fa3833bbf2124f9cc55c6d4154a2216e7ef4
Endel Dreyer <endel.dreyer@gmail.com>
2013-12-14 12:57:53 -0200

    testing collection internals

--- client/tests/pagination.js
+++ client/tests/pagination.js
@@ -5,13 +5,13 @@ asyncTest("Pagination", function() {
   posts.orderBy('created_at', -1);
 
   posts.paginate(function(pagination) {
-    ok(pagination.isFetching() == false);
-    ok(pagination.per_page === 50);
-    ok(pagination.current_page === 1);
-    ok(pagination.last_page >= 1);
-    ok(pagination.from == 1);
-    ok(pagination.to >= 1);
-    ok(pagination.data.length > 0);
+    ok(pagination.isFetching() == false, "is fetching");
+    ok(pagination.per_page === 50, "per_page");
+    ok(pagination.current_page === 1, "current_page");
+    ok(pagination.last_page >= 1, "last_page");
+    ok(pagination.from == 1, "from");
+    ok(pagination.to >= 1, "to");
+    ok(pagination.data.length > 0, "data");
 
     start();
   });
