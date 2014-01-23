5497af877968e8c9fbd2783c3be181d9b7815e66
Endel Dreyer <endel.dreyer@gmail.com>
2014-01-23 01:45:32 -0200

    playing with EventSource. not functional

--- api/app/middlewares/auth.php
+++ api/app/middlewares/auth.php
@@ -8,15 +8,18 @@ class AuthMiddleware extends \Slim\Middleware
 		// The Slim application
 		$app = $this->app;
 
+		$referer = parse_url($app->environment->offsetGet('HTTP_REFERER'));
+
 		// Enable Cross-Origin Resource Sharing
-		$app->response->headers->set('Access-Control-Allow-Origin', '*');
+		$app->response->headers->set('Access-Control-Allow-Origin', (isset($referer['host'])) ? 'http://'.$referer['host'] : '*' );
+		$app->response->headers->set('Access-Control-Allow-Credentials', 'true');
 		$app->response->headers->set('Access-Control-Allow-Method', 'GET, PUT, POST, DELETE');
 		$app->response->headers->set('Access-Control-Allow-Headers', 'x-app-id, x-app-key, content-type, user-agent');
 
 		// Don't proceed on CORS requests.
 		if (!$app->request->isOptions()) {
-			$app->key = Models\AppKey::where('app_id', $app->request->headers->get('X-App-Id'))
-				->where('key', $app->request->headers->get('X-App-Key'))
+			$app->key = Models\AppKey::where('app_id', $app->request->headers->get('X-App-Id') ?: $app->request->get('X-App-Id'))
+				->where('key', $app->request->headers->get('X-App-Key') ?: $app->request->get('X-App-Key'))
 				->first();
 
 			$app->response->headers->set('Content-type', 'application/json');
