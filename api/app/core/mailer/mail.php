6cd667c19dd2c5ddb00836fc35aae424a1a79e03
Endel Dreyer <endel.dreyer@gmail.com>
2014-02-13 21:24:21 -0200

    add mail module

--- api/app/core/mailer/mail.php
+++ api/app/core/mailer/mail.php
@@ -1,10 +1,27 @@
 <?php
 
-namespace Mailer;
-
 class Mail {
 
-	public static function send($options = array()) {
+	public static function getTransport($params = array()) {
+		// Validate SMTP params
+		if ($params['driver'] == 'smtp' && (!isset($params['username']) || !isset($params['password']))) {
+			throw new Exception("'mail.username' and 'mail.password' configs are required when using 'smtp' driver;");
+		}
+
+		$transport_klass = '\Swift_'.ucfirst(strtolower($params['driver'])).'Transport';
+		$transport = $transport_klass::newInstance();
+		unset($params['driver']);
+
+		// Set custom transport params
+		foreach($params as $param => $value) {
+			$transport->{'set'.ucfirst($param)}($value);
+		}
+
+		return $transport;
+	}
+
+	public static function sendMessage($transport, $options) {
+		// Validate options
 		if (!isset($options['to'])) {
 			throw new \Exception(__CLASS__ . "::".__METHOD__.": 'to' option is required.");
 		}
@@ -13,8 +30,40 @@ class Mail {
 			throw new \Exception(__CLASS__ . "::".__METHOD__.": 'template' option is required.");
 		}
 
+		$mailer = \Swift_Mailer::newInstance($transport);
+
+		$message = \Swift_Message::newInstance($options['subject'])
+			->setFrom($options['from'])
+			->setTo($options['to'])
+			->setBody($options['template']);
+
+		return $mailer->send($message);
+	}
+
+	public static function send($options = array()) {
 		$app = \Slim\Slim::getInstance();
-		$app->config('mail')
+		$mail_configs = $app->key->app->configs()->where('name', 'like', 'mail.%')->get();
+
+		$params = array();
+		$mail_configs->each(function($config) use (&$params) {
+			preg_match('/mail\.([a-z]+)/', $config->name, $matches);
+			$params[ $matches[1] ] = $config->value;
+		});
+
+		// set 'mail' as default driver
+		if (!isset($params['driver'])) {
+			$params['driver'] = 'mail';
+		} else {
+
+			$preset_file = __DIR__ . '/presets/' . $params['driver'] . '.php';
+			if (file_exists($preset_file)) {
+				$params = array_merge($params, require($preset_file));
+			}
+
+		}
+
+		$transport = static::getTransport($params);
+		return static::sendMessage($transport, $options);
 	}
 
 }
