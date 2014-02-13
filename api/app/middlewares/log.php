e2b04a0200765e8ab826e7038a486db63719a63b
Endel Dreyer <endel.dreyer@gmail.com>
2014-02-13 18:23:55 -0200

    disable request_logs temporarily

--- api/app/middlewares/log.php
+++ api/app/middlewares/log.php
@@ -7,12 +7,12 @@ class LogMiddleware extends \Slim\Middleware
 		$app = $this->app;
 
 		if (!$app->request->isOptions() && $app->key) {
-			\models\RequestLog::create(array(
-				'app_id' => $app->key->app_id,
-				'key_id' => $app->key->_id,
-				'uri' => $app->request->getResourceUri(),
-				'method' => $app->request->getMethod()
-			));
+			// \models\RequestLog::create(array(
+			// 	'app_id' => $app->key->app_id,
+			// 	'key_id' => $app->key->_id,
+			// 	'uri' => $app->request->getResourceUri(),
+			// 	'method' => $app->request->getMethod()
+			// ));
 		}
 
 		$this->next->call();
