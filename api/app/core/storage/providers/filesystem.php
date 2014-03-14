da04431bd8b50a60f85c7577eb7671557d474c1e
Endel Dreyer <endel.dreyer@gmail.com>
2014-03-14 20:05:05 -0300

    work in progress with multiple file upload per collection

--- api/app/core/storage/providers/filesystem.php
+++ api/app/core/storage/providers/filesystem.php
@@ -3,15 +3,18 @@ namespace Storage\Providers;
 
 class Filesystem extends Base {
 
-	public function upload($path, $file, $options=array()) {
+	public function upload($file, $options=array()) {
 		$filename = md5($file['name']) . uniqid() . "." . pathinfo($file['name'], PATHINFO_EXTENSION);
-		if(!is_dir($path)){
-			mkdir($path, 0777, true);
+		$public_dir = 'storage/files/';
+		$dir = __DIR__ . '/../../../' . $public_dir . $app->key->app_id;
+
+		if (!is_dir($dir)) {
+			mkdir($dir, 0777, true);
 		}
-		$dest = $path . '/' . $filename;
-		if(move_uploaded_file($file['tmp_name'], $dest)){
-			return $filename;
-		}else{
+
+		if(move_uploaded_file($file['tmp_name'], $dir . '/' . $filename)){
+			return '//' . $_SERVER['SERVER_NAME'] . '/app/' . $public_dir . $filename;
+		} else {
 			return NULL;
 		}
 	}
