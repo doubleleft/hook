406e1d95b296a9313d2348a730c51858950021bc
Endel Dreyer <endel.dreyer@gmail.com>
2014-05-03 01:18:50 -0300

    add KeyValue tests, with some unimplemented feature requests.

---
+++ api/tests/integration/key_value/KeyValueTest.php
@@ -0,0 +1,30 @@
+<?php
+
+class KeyValue extends TestCase {
+
+	public function testSetString() {
+		$this->post('key/some_string', array('value' => 5));
+		$key = $this->get('key/some_string');
+		$this->assertTrue($key['value'] === '5');
+	}
+
+	public function testSetInteger() {
+		$this->post('key/some_integer', array('value' => 5));
+		$key = $this->get('key/some_integer');
+		$this->assertTrue($key['value'] === 5);
+	}
+
+	public function testSetFloat() {
+		$this->post('key/some_float', array('value' => 9.9));
+		$key = $this->get('key/some_float');
+		$this->assertTrue($key['value'] === 9.9);
+	}
+
+	public function testSetArray() {
+		$this->post('key/some_array', array('value' => array('one', 'two', 'three')));
+		$key = $this->get('key/some_array');
+		$this->assertTrue($key['value'] === array('one', 'two', 'three'));
+	}
+
+}
+
