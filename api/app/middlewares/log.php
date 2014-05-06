e2151158ac4a52383bfc0d2ac47086572377e37d
Endel Dreyer <endel.dreyer@gmail.com>
2014-05-06 14:57:45 -0300

    fix instantiating log writer in global route context. #37

--- api/app/middlewares/log.php
+++ api/app/middlewares/log.php
@@ -5,11 +5,15 @@ class LogMiddleware extends \Slim\Middleware
 	public function call()
 	{
 		$app = $this->app;
-		// set application log writer for this app
-		$this->app->log->setWriter(new \LogWriter(storage_dir() . '/logs.txt'));
 
-		if ($app->request->getResourceUri() !== '/apps/logs') {
-			if (!$app->request->isOptions() && $app->key) {
+		//
+		// TODO: find a way to enable/disable logs for production use
+		//
+		if (!$app->request->isOptions() && $app->key) {
+			// set application log writer for this app
+			$this->app->log->setWriter(new \LogWriter(storage_dir() . '/logs.txt'));
+
+			if ($app->request->getResourceUri() !== '/apps/logs') {
 				$log = $app->request->getIp() . ' - [' . date('d-m-Y H:i:s') . '] ';
 				$log .= $app->request->getMethod() . ' ' . $app->request->getResourceUri() . PHP_EOL;
 				$log .= 'Params: ' . json_encode($app->request->params()) . PHP_EOL;
