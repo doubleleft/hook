e0646c768726bafd7302e7a9bbbbfed0d65efa42
Endel Dreyer <endel.dreyer@gmail.com>
2014-05-08 13:50:49 -0300

    simple working websockets version

--- api/channel.php
+++ api/channel.php
@@ -21,42 +21,38 @@ class Channel implements WampServerInterface {
 
 	public function onPublish(ConnectionInterface $conn, $topic, $event, array $exclude, array $eligible) {
 		echo "publish..." . PHP_EOL;
-		var_dump($topic);
-		var_dump($event);
+		// var_dump($conn);
+		// var_dump($topic);
+		// var_dump($event);
+		// var_dump($exclude);
+		// var_dump($eligible);
 		$topic->broadcast($event);
 	}
 
-	public function onSubscribe(ConnectionInterface $conn, $topic) { }
-	public function onUnSubscribe(ConnectionInterface $conn, $topic) { }
+	public function onSubscribe(ConnectionInterface $conn, $topic) {}
+	public function onUnSubscribe(ConnectionInterface $conn, $topic) {}
 
 	public function onOpen(ConnectionInterface $conn) {
-		echo "openned..." . PHP_EOL;
- 	}
+		echo "opened..." . PHP_EOL;
+	}
+
 	public function onClose(ConnectionInterface $conn) {
 		echo "closed..." . PHP_EOL;
- 	}
+	}
+
 	public function onError(ConnectionInterface $conn, \Exception $e) {
 		var_dump($e);
- 	}
+	}
 }
 
  // Set up our WebSocket server for clients wanting real-time updates
 $loop = React\EventLoop\Factory::create();
+
 $socket_server = new React\Socket\Server($loop);
-$socket_server->listen(8889, '0.0.0.0'); // Binding to 0.0.0.0 means remotes can connect
-$io_server = new Ratchet\Server\IoServer(
-	new Ratchet\Http\HttpServer(
-		new Ratchet\WebSocket\WsServer(
-			new Ratchet\Wamp\WampServer(
-				new Channel()
-			)
-		)
-	),
-	$socket_server
-);
+$socket_server->listen(8080, '0.0.0.0'); // Binding to 0.0.0.0 means remotes can connect
+
+$io_server = new Ratchet\Server\IoServer(new Ratchet\Http\HttpServer(
+	new Ratchet\WebSocket\WsServer(new Ratchet\Wamp\WampServer(new Channel()))
+), $socket_server);
 
 $loop->run();
-
-// $server = new \Ratchet\App('localhost', 8889);
-// $server->route('/', new Channel());
-// $server->run();
