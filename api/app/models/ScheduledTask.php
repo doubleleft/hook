1612f2d9a93eddd823a8d85217692f3589514782
Endel Dreyer <endel.dreyer@gmail.com>
2014-05-09 18:00:31 -0300

    assume that scheduled tasks will always run on port 80

--- api/app/models/ScheduledTask.php
+++ api/app/models/ScheduledTask.php
@@ -29,7 +29,8 @@ class ScheduledTask extends \Core\Model
 		$schedule = preg_match('/[a-z]/', $this->schedule) ? $shortcuts[$this->schedule] : $this->schedule;
 
 		$protocol = (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https' : 'http');
-		$public_url = $protocol . '://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['SCRIPT_NAME'] . '/' . $this->task;
+		// $public_url = $protocol . '://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['SCRIPT_NAME'] . '/' . $this->task;
+		$public_url = $protocol . '://' . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'] . '/' . $this->task;
 
 		// TODO: redirect output to application log file.
 		// https://github.com/doubleleft/dl-api/issues/37
