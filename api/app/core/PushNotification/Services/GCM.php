32b15604c6c6b9b18944efcc703db53761c526db
Endel Dreyer <endel.dreyer@gmail.com>
2014-05-20 20:44:45 -0300

    add reference implementation as comment for Android/GCM. #33

--- api/app/core/PushNotification/Services/GCM.php
+++ api/app/core/PushNotification/Services/GCM.php
@@ -3,7 +3,13 @@ namespace PushNotification\Services;
 
 class GCM {
 
-	public function send($data) {
+	/**
+	 * push
+	 * @param mixed $registrations
+	 * @param mixed $data
+	 */
+	public function push($registrations, $data) {
+		// https://gist.github.com/prime31/5675017
 	}
 
 }
