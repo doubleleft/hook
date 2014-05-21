916238bab6a95038dac82865a70b0ca8727aa6f0
Endel Dreyer <endel.dreyer@gmail.com>
2014-05-21 19:17:17 -0300

    improve the way to upload p12/pem certificates. #33

--- commandline/src/commands/config_set.php
+++ commandline/src/commands/config_set.php
@@ -10,9 +10,33 @@ return array(
 		foreach($args as $arg) {
 			if (!is_null($arg) && preg_match('/=/', $arg)) {
 				$config = preg_split('/=/', $arg);
+				$name = $config[0];
+				$value = $config[1];
+
+				//
+				// Read or extract certificate file
+				// --------------------------------
+				//
+				if (file_exists($value)) {
+					\Client\Console::output("Reading certificate file..." . PHP_EOL);
+
+					$ext = pathinfo($value, PATHINFO_EXTENSION);
+					if ($ext == 'p12') {
+						$results = array();
+						$worked = openssl_pkcs12_read(file_get_contents($value), $results, null);
+						if ($worked) {
+							$value = $results['cert'] . $results['pkey'];
+						} else {
+							\Client\Console::error(openssl_error_string());
+						}
+					} else if ($ext == 'pem') {
+						$value = file_get_contents($value);
+					}
+				}
+
 				array_push($configs_to_add, array(
-					'name' => $config[0],
-					'value' => $config[1]
+					'name' => $name,
+					'value' => $value
 				));
 			}
 		}
