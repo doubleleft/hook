ab3f8588c9b6f4010be062585710c2949293a196
Endel Dreyer <endel.dreyer@gmail.com>
2014-05-27 18:02:33 -0300

    use TEXT on AppConfig's value attribute

--- api/app/models/schema/04_create_app_config_table.php
+++ api/app/models/schema/04_create_app_config_table.php
@@ -4,7 +4,7 @@ return array('app_configs' => function($t) {
 	$t->increments('_id');
 	$t->integer('app_id')->references('_id')->on('apps');
 	$t->string('name');
-	$t->string('value');
+	$t->text('value');
 
 	// timestamps
 	$t->integer('created_at');
