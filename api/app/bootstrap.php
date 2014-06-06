3050df83805728df809f93f58912f78ca10e3ad5
Endel Dreyer <endel.dreyer@gmail.com>
2014-06-06 13:55:45 -0300

    remove deprecated duccio/APNS autoloader fix. add tests for push notification

--- api/app/bootstrap.php
+++ api/app/bootstrap.php
@@ -11,24 +11,4 @@ require __DIR__ . '/bootstrap/connection.php';
 // core
 require __DIR__ . '/core/functions.php';
 
-//
-// WARNING: MonkeyPatch on ApnsPHP autoloader
-//
-// Need to update this when the pull request is accepted:
-// https://github.com/duccio/ApnsPHP/pull/15
-//
-spl_autoload_unregister('ApnsPHP_Autoload');
-function ApnsPHP_Autoload_Patched($sClassName) {
-	if (empty($sClassName)) { throw new Exception('Class name is empty'); }
-
-	$sPath = dirname(dirname(__FILE__)) . '/vendor/duccio/apns-php';
-	if (empty($sPath)) { throw new Exception('Current path is empty'); }
-
-	$sFile = sprintf('%s%s%s.php', $sPath, DIRECTORY_SEPARATOR, str_replace('_', DIRECTORY_SEPARATOR, $sClassName));
-	if (is_file($sFile) && is_readable($sFile)) {
-		require_once $sFile;
-	}
-}
-spl_autoload_register('ApnsPHP_Autoload_Patched');
-
 return $app;
