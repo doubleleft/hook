8d315ef6d08607a0c8a8021f9eb53d94e4b115e8
Endel Dreyer <endel.dreyer@gmail.com>
2014-06-13 21:44:35 -0300

    fix key-value tests

--- api/tests/integration/key_value/KeyValueTest.php
+++ api/tests/integration/key_value/KeyValueTest.php
@@ -8,26 +8,24 @@ class KeyValue extends TestCase {
 		$this->assertTrue($key['value'] === '5');
 	}
 
-// 	// TODO
-// 	public function testSetInteger() {
-// 		$this->post('key/some_integer', array('value' => 5));
-// 		$key = $this->get('key/some_integer');
-// 		$this->assertTrue($key['value'] === 5);
-// 	}
-//
-// 	// TODO
-// 	public function testSetFloat() {
-// 		$this->post('key/some_float', array('value' => 9.9));
-// 		$key = $this->get('key/some_float');
-// 		$this->assertTrue($key['value'] === 9.9);
-// 	}
-//
-// 	// TODO
-// 	public function testSetArray() {
-// 		$this->post('key/some_array', array('value' => array('one', 'two', 'three')));
-// 		$key = $this->get('key/some_array');
-// 		$this->assertTrue($key['value'] === array('one', 'two', 'three'));
-// 	}
+	public function testSetInteger() {
+		$this->post('key/some_integer', array('value' => 5));
+		$key = $this->get('key/some_integer');
+		$this->assertTrue($key['value'] === '5', 'int 5 results as "5"');
+	}
+
+	public function testSetFloat() {
+		$this->post('key/some_float', array('value' => 9.9));
+		$key = $this->get('key/some_float');
+		$this->assertTrue($key['value'] === '9.9', 'float 9.9 results as "9.9"');
+	}
+
+	public function testSetArray() {
+		$response = $this->post('key/some_array', array('value' => array('one', 'two', 'three')));
+		$this->assertTrue(isset($response['error']), "array key-values are not supported.");
+		// $key = $this->get('key/some_array');
+		// $this->assertTrue($key['value'] === array('one', 'two', 'three'));
+	}
 
 }
 
