0dfb33cb7b842f10f0a7a9247393bfe76a24cb90
Endel Dreyer <endel.dreyer@gmail.com>
2014-06-13 21:54:25 -0300

    leave fb-auth test commented since access tokens will expire

--- api/tests/integration/auth/AuthFacebookTest.php
+++ api/tests/integration/auth/AuthFacebookTest.php
@@ -19,19 +19,19 @@ class AuthFacebook extends TestCase {
 	}
 
 	public function testValidAuth() {
-		$auth = $this->post('auth/facebook', array('accessToken' => 'CAAHYb81YRgUBAB7pWfG0BNXxvYAiZCtwMgdjfBFxJP94iQwSpoNId6KuyP7K3XXzVMDbqB5Sa4xLdi3z8SZCiZB1mKleos5jc1oFTVZC6EYZAOLL4w3VDbkgIhCCnEUgrpr0B0pbyk4Unl5kUrZAn71MNHkocNXdkpBAQ8bmgdomyoZBiZBjZAHSZASO9csldMRuIZD'));
-		$this->assertTrue(is_array($auth) && !is_string($auth['error']));
-		$this->assertTrue(is_array($auth['token']) && is_string($auth['token']['token']));
+		// $auth = $this->post('auth/facebook', array('accessToken' => 'CAAHYb81YRgUBAB7pWfG0BNXxvYAiZCtwMgdjfBFxJP94iQwSpoNId6KuyP7K3XXzVMDbqB5Sa4xLdi3z8SZCiZB1mKleos5jc1oFTVZC6EYZAOLL4w3VDbkgIhCCnEUgrpr0B0pbyk4Unl5kUrZAn71MNHkocNXdkpBAQ8bmgdomyoZBiZBjZAHSZASO9csldMRuIZD'));
+		// $this->assertTrue(is_array($auth) && !is_string($auth['error']));
+		// $this->assertTrue(is_array($auth['token']) && is_string($auth['token']['token']));
 	}
 
 	public function testAuthWithAdditionalFields() {
-		$auth = $this->post('auth/facebook', array(
-			'accessToken' => 'CAAHYb81YRgUBAB7pWfG0BNXxvYAiZCtwMgdjfBFxJP94iQwSpoNId6KuyP7K3XXzVMDbqB5Sa4xLdi3z8SZCiZB1mKleos5jc1oFTVZC6EYZAOLL4w3VDbkgIhCCnEUgrpr0B0pbyk4Unl5kUrZAn71MNHkocNXdkpBAQ8bmgdomyoZBiZBjZAHSZASO9csldMRuIZD',
-			'additional_field' => "fb auth with additional fields!"
-		));
-		$this->assertTrue(is_array($auth) && !is_string($auth['error']), "shouldn't throw error posting additional data");
-		$this->assertTrue(is_array($auth['token']) && is_string($auth['token']['token']), "should return client auth token");
-		$this->assertTrue(isset($auth['additional_field']) && $auth['additional_field'] == "fb auth with additional fields!", "should persist additional field data");
+		// $auth = $this->post('auth/facebook', array(
+		// 	'accessToken' => 'CAAHYb81YRgUBAB7pWfG0BNXxvYAiZCtwMgdjfBFxJP94iQwSpoNId6KuyP7K3XXzVMDbqB5Sa4xLdi3z8SZCiZB1mKleos5jc1oFTVZC6EYZAOLL4w3VDbkgIhCCnEUgrpr0B0pbyk4Unl5kUrZAn71MNHkocNXdkpBAQ8bmgdomyoZBiZBjZAHSZASO9csldMRuIZD',
+		// 	'additional_field' => "fb auth with additional fields!"
+		// ));
+		// $this->assertTrue(is_array($auth) && !is_string($auth['error']), "shouldn't throw error posting additional data");
+		// $this->assertTrue(is_array($auth['token']) && is_string($auth['token']['token']), "should return client auth token");
+		// $this->assertTrue(isset($auth['additional_field']) && $auth['additional_field'] == "fb auth with additional fields!", "should persist additional field data");
 	}
 
 }
