ec98b761749194ec945e24dc6913df041147df4c
Endel Dreyer <endel.dreyer@gmail.com>
2014-07-30 19:47:16 -0300

    fix namespaces at AuthController

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -1,12 +1,15 @@
 <?php namespace Hook\Controllers;
 
+use Hook\Model\Auth;
+use Hook\Auth\Provider as AuthProvider;
+
 class AuthController extends HookController {
     public function show() {
-        return $this->json(Model\Auth::current());
+        return $this->json(Auth::current());
     }
 
     public function execute($provider_name, $method = 'register') {
-        return $this->json(Auth\Provider::get($provider_name)->{$method}(CollectionController::getData()));
+        return $this->json(AuthProvider::get($provider_name)->{$method}(CollectionController::getData()));
     }
 }
 
