f6b8441c4384f9a56ec888e2739697c92bff0e96
Endel Dreyer <endel.dreyer@gmail.com>
2014-08-01 13:10:21 -0300

    create point/spatial index on postgres. #82

--- src/Database/Schema/Grammars/PostgresGrammar.php
+++ src/Database/Schema/Grammars/PostgresGrammar.php
@@ -5,5 +5,33 @@ use Illuminate\Database\Schema\Grammars as IlluminateGrammars;
 use Illuminate\Support\Fluent;
 use Illuminate\Database\Schema\Blueprint;
 
-class PostgresGrammar extends IlluminateGrammars\PostgresGrammar { }
+class PostgresGrammar extends IlluminateGrammars\PostgresGrammar {
+
+    /**
+     * Create the column definition for a timestamp type.
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typePoint(Fluent $column)
+    {
+        return 'point';
+    }
+
+    /**
+     * Compile a plain index key command.
+     *
+     * @param  \Illuminate\Database\Schema\Blueprint  $blueprint
+     * @param  \Illuminate\Support\Fluent  $command
+     * @return string
+     */
+    public function compileSpatial(Blueprint $blueprint, Fluent $command)
+    {
+        $columns = $this->columnize($command->columns);
+
+        return "create index {$command->index} on ".$this->wrapTable($blueprint)." using gist({$columns})";
+    }
+
+
+}
 
