b665a9e36b38fa758ecdd95df34c58916008e304
Endel Dreyer <endel.dreyer@gmail.com>
2014-08-08 17:46:55 -0300

    don't run composer on deployment when packages haven't changed.

--- src/Package/Manager.php
+++ src/Package/Manager.php
@@ -2,44 +2,43 @@
 
 use Hook\Database\AppContext;
 
+use Composer\Factory;
 use Composer\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\StreamOutput;
+use Symfony\Component\Console\Formatter\OutputFormatter;
 
 class Manager {
     const VENDOR_DIR = 'vendor';
     const VENDOR_TMP_DIR = 'vendor-tmp';
 
     public static function install($packages) {
-        // if (empty($packages)) { return false; }
+        // Don't proceed if packages haven't changed.
+        if ($packages == self::dump()) { return false; }
 
         putenv('COMPOSER_HOME=' . __DIR__ . '/../../vendor/bin/composer');
         self::createComposerJson($packages);
         chdir(storage_dir());
+
+        // Setup composer output formatter
+        $stream = fopen('php://temp', 'w+');
+        $output = new StreamOutput($stream);
 
         //
         // Programmatically run `composer install`
         //
         $application = new Application();
         $application->setAutoExit(false);
-        $code = $application->run(new ArrayInput(array('command' => 'install')));
-
-        // remove previously installed packages
-        if (file_exists(storage_dir() . '/' . self::VENDOR_DIR)) {
-            rmdir_r(storage_dir() . '/' . self::VENDOR_DIR);
-        }
-
-        // move newsly installed packages to `production`
-        if (file_exists(storage_dir() . '/' . self::VENDOR_TMP_DIR)) {
-            rename(
-                storage_dir() . '/' . self::VENDOR_TMP_DIR,
-                storage_dir() . '/' . self::VENDOR_DIR
-            );
-        }
+        $code = $application->run(new ArrayInput(array('command' => 'install')), $output);
 
         // remove composer.json
-        unlink(storage_dir() . '/composer.json');
+        if (file_exists(storage_dir() . '/composer.lock')) {
+            unlink(storage_dir() . '/composer.lock');
+        }
 
-        return $code == 0;
+        // rewind stream to read full contents
+        rewind($stream);
+        return stream_get_contents($stream);
     }
 
     public static function dump() {
@@ -61,7 +60,7 @@ class Manager {
 
     protected static function createComposerJson($packages) {
         $composer_json = str_replace("\/", '/', json_encode(array(
-            'config' => array('vendor-dir' => self::VENDOR_TMP_DIR),
+            'config' => array('vendor-dir' => self::VENDOR_DIR),
             'require' => $packages
         )));
         return file_put_contents(storage_dir() . '/composer.json', $composer_json);
