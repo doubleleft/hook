f394f0d74f443c910d0f8c9ab3d79b047776ef3b
Endel Dreyer <endel.dreyer@gmail.com>
2014-08-11 19:52:38 -0300

    add 'view' method to default hook controller

--- src/Controllers/HookController.php
+++ src/Controllers/HookController.php
@@ -13,10 +13,25 @@ class HookController {
         Router::getInstance()->hook('slim.after.dispatch', $callback);
     }
 
+    /**
+     * json
+     *
+     * @param mixed $data
+     */
     protected function json($data) {
         $response = Router::getInstance()->response;
         $response->headers->set('Content-type', 'application/json');
         $response->setBody(to_json($data));
+    }
+
+    /**
+     * view
+     *
+     * @param mixed $template
+     * @param array $data
+     */
+    protected function view($template, $data = array()) {
+        Router::getInstance()->render($template, $data);
     }
 
 }
