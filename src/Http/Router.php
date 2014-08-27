8ffabc5e2d35b119aec2b4da583ea26027f3ce6b
Endel Dreyer <endel.dreyer@gmail.com>
2014-08-27 00:59:31 -0300

    improve mounting controllers routes

--- src/Http/Router.php
+++ src/Http/Router.php
@@ -97,19 +97,21 @@ class Router {
                 continue;
             }
 
-            // // call 'mounted' method
+            // call 'mounted' method
             if ($method_name == 'mounted') {
                 call_user_func(array($controller_klass, 'mounted'), $path);
                 continue;
             }
 
-            preg_match('/^(get|put|post|patch)/', $method_name, $matches);
+            preg_match_all('/^(get|put|post|patch)(.*)/', $method_name, $matches);
+            $has_matches = (count($matches[1]) > 0);
 
-            $http_method = (count($matches) > 0) ? $matches[0] : 'any';
-            $route = "{$path}";
+            $http_method = $has_matches ? $matches[1][0] : 'any';
+            $route_name = $has_matches ? $matches[2][0] : $method_name;
 
-            if ($method_name !== 'index') {
-                $route .= '/' . $method_name;
+            $route = str_finish($path, '/');
+            if ($route_name !== 'index') {
+                $route .= snake_case($route_name);
             }
 
             static::$instance->{$http_method}($route, "{$controller_klass}:{$method_name}");
