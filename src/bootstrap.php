97881f05430b26808ec7f48d11fdd977f36c5b9c
Endel Dreyer <endel.dreyer@gmail.com>
2014-10-07 23:48:09 -0300

    fix setting global aliases

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,4 +5,9 @@ $app->config('database', require(__DIR__ . '/../config/database.php'));
 $app->config('paths', require(__DIR__ . '/../config/paths.php'));
 
 require __DIR__ . '/bootstrap/connection.php';
+
+foreach($app->config('aliases') as $alias => $source) {
+    class_alias($source, $alias);
+}
+
 return $app;
