c220db8fc78c88688d4bc820c76fc14ef1710e7e
Gilson Nakazato <gil.nak@gmail.com>
2014-10-20 19:17:48 -0200

    fixing hostname

--- salt/hook/database.php
+++ salt/hook/database.php
@@ -2,11 +2,12 @@
 {% set mysql_user = proj_name|replace('-','')|truncate(15) -%}
 {% set mysql_pass = salt['grains.get']('mysql:' ~ mysql_user ~ '') -%}
 {% set mysql_db = mysql_user -%}
+{% set mysql_host = salt['grains.get']('mysql.host','localhost') -%}
 <?php
 
 return array(
 		'driver'   => 'mysql',
-		'host'     => 'localhost',
+		'host'     => '{{ mysql_host }}',
 		'username' => '{{ mysql_user }}',
 		'password' => '{{ mysql_pass }}',
 		'database' => '{{ mysql_db }}',
