ec89468aeafa5af09dee6de4da99e87d1725b4e6
Aline Freitas <a.freitas@doubleleft.com>
2014-10-28 16:41:51 -0200

    fix enclosing jinja tag to prevent whitespaces.

--- salt/hook/database.php
+++ salt/hook/database.php
@@ -1,9 +1,9 @@
-<?php
 {% set proj_name = salt['pillar.get']('proj_name','myproject') -%}
 {% set mysql_user = proj_name|replace('-','')|truncate(15) -%}
 {% set mysql_pass = salt['grains.get']('mysql:' ~ mysql_user ~ '') -%}
 {% set mysql_db = mysql_user -%}
-{% set mysql_host = salt['pillar.get']('master:mysql.host','localhost') %}
+{% set mysql_host = salt['pillar.get']('master:mysql.host','localhost') -%}
+<?php
 
 return array(
 		'driver'   => 'mysql',
