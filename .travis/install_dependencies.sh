5a8ce1e5ff3376239832b93239cbe137379e23c6
Endel Dreyer <endel.dreyer@gmail.com>
2014-11-04 13:00:01 -0200

    use nginx instead of apache2 as webserver on travis

--- .travis/install_dependencies.sh
+++ .travis/install_dependencies.sh
@@ -1,50 +1,26 @@
 #!/usr/bin/env bash
 
-# Install everything
-sudo apt-get install -y --force-yes apache2 libapache2-mod-php5 php5-mysql php5-sqlite
+# server stack
+sudo apt-get update -y -q
+sudo apt-get install nginx
 
-# Configure Apache
-WEBROOT="$(pwd)/public"
-CGIROOT=`dirname "$(which php-cgi)"`
-echo "WEBROOT: $WEBROOT"
-echo "CGIROOT: $CGIROOT"
-sudo echo "<VirtualHost *:80>
-        DocumentRoot $WEBROOT
-        <Directory />
-                Options FollowSymLinks
-                AllowOverride All
-        </Directory>
-        <Directory $WEBROOT >
-                Options Indexes FollowSymLinks MultiViews
-                AllowOverride All
-                Order allow,deny
-                allow from all
-        </Directory>
+# enable php-fpm
+cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
-		# Configure PHP as CGI
-		ScriptAlias /local-bin $CGIROOT
-		DirectoryIndex index.php index.html
-		AddType application/x-httpd-php5 .php
-		Action application/x-httpd-php5 '/local-bin/php-cgi'
+# configure nginx
+sudo sed -i -e"s/user www-data;/user root;/" /etc/nginx/nginx.conf
+sudo sed -i -e"s/keepalive_timeout\s*65/keepalive_timeout 2/" /etc/nginx/nginx.conf
+sudo sed -i -e"s/keepalive_timeout 2/keepalive_timeout 2;\n\tclient_max_body_size 3m/" /etc/nginx/nginx.conf
+cat ./.travis/nginx.conf | sed -e "s,%TRAVIS_BUILD_DIR%,`pwd`/public,g" | sudo tee /etc/nginx/sites-available/default > /dev/null
+sudo service nginx restart
 
-</VirtualHost>" | sudo tee /etc/apache2/sites-available/default > /dev/null
-cat /etc/apache2/sites-available/default
-
-sudo a2enmod rewrite
-sudo a2enmod actions
-sudo service apache2 restart
-
-# apply permissions on writable directories
+# apply server permissions
 sudo chown -R www-data shared
 sudo chown -R www-data public/storage
-
-# mysql -e 'CREATE DATABASE dlapi;'
-# sed s/%database_name%/myapp_test/ app/config/parameters.ini-dist | sed s/%database_login%/root/ | sed s/%database_password%// > app/config/parameters.ini
 
 # Configure custom domain
 echo "127.0.0.1 hook.dev" | sudo tee --append /etc/hosts
 
 # create default app
 curl -XPOST http://hook.dev/public/index.php/apps --data '{"app":{"name":"travis"}}' > tests/app.json
-
-echo "TRAVIS_PHP_VERSION: $TRAVIS_PHP_VERSION"
