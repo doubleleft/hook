751e09a6d7213e8eb3dd8c417d391110410a99d2
Endel Dreyer <endel.dreyer@gmail.com>
2014-11-04 15:57:46 -0200

    let sqlite db driver enabled by default

--- config/database.php
+++ config/database.php
@@ -2,21 +2,21 @@
 
 return array(
 
-    // // SQLite
-    // // -------
-    // 'driver'   => 'sqlite',
-    // 'database' => __DIR__ . '/../shared/database.sqlite',
-    // 'prefix'   => '',
-
-    // MySQL
+    // SQLite
     // -------
-    'driver'   => 'mysql',
-    'host'     => '127.0.0.1',
-    'username' => 'root',
-    'password' => '',
-    'database' => 'dlapi',
-    'collation' => 'utf8_general_ci',
-    'charset' => 'utf8'
+    'driver'   => 'sqlite',
+    'database' => __DIR__ . '/../shared/database.sqlite',
+    'prefix'   => '',
+
+    // // MySQL
+    // // -------
+    // 'driver'   => 'mysql',
+    // 'host'     => '127.0.0.1',
+    // 'username' => 'root',
+    // 'password' => '',
+    // 'database' => 'dlapi',
+    // 'collation' => 'utf8_general_ci',
+    // 'charset' => 'utf8'
 
     // // PostgreSQL
     // // -------
