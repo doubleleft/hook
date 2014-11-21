e3c0450078dbacf2bbb93897c03658d88c393029
Endel Dreyer <endel.dreyer@gmail.com>
2014-11-21 01:38:02 -0200

    add testcase for issue #106.

---
+++ tests/unit/models/CollectionJsonTest.php
@@ -0,0 +1,24 @@
+<?php
+use Hook\Model\App;
+
+class CollectionJsonTest extends TestCase
+{
+
+    public function testDataTypes()
+    {
+        App::collection('dummy')->create(array(
+            'boolean' => true,
+            'number' => 1,
+            'float' => 5.5,
+            'string' => "Strings"
+        ));
+
+        $data = App::collection('dummy')->first()->toArray();
+
+        $this->assertTrue(is_int($data['number']));
+        $this->assertTrue(is_float($data['float']));
+        $this->assertTrue(is_string($data['string']));
+        $this->assertTrue(is_bool($data['boolean']));
+    }
+
+}
