32fd0d9d3860da8a5a458b40782af701b68eb7a8
Endel Dreyer <endel.dreyer@gmail.com>
2015-01-04 15:25:34 -0200

    clone with depth=1 on installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/doubleleft/hook.git
+git clone https://github.com/doubleleft/hook.git --depth=1
 cd hook
 make
 cd ../
 
-git clone https://github.com/doubleleft/hook-cli.git
+git clone https://github.com/doubleleft/hook-cli.git --depth=1
 cd hook-cli
 make
 cd ../
