834cf73712300b5696af38ea8869fff9033cc13e
Endel Dreyer <endel.dreyer@gmail.com>
2015-01-12 12:02:32 -0200

    break router into application routes. don't mount core routes automatically.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,6 +4,8 @@ $app = require __DIR__ . '/Hook.php';
 $app->config('database', require(__DIR__ . '/../config/database.php'));
 $app->config('paths', require(__DIR__ . '/../config/paths.php'));
 
+Hook\Http\Router::setInstance($app);
+
 require __DIR__ . '/bootstrap/connection.php';
 
 foreach($app->config('aliases') as $alias => $source) {
