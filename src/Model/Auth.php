3dc751b99957a411bf94ac48a607cd2a5cb7927f
Fernando França <fefranca@doubleleft.com>
2015-02-27 21:51:53 -0300

    Allow isTrustedAction to be read outside of model (OAuth fix)

--- src/Model/Auth.php
+++ src/Model/Auth.php
@@ -27,7 +27,7 @@ class Auth extends Collection
 
     // force a trusted action?
     // - currently only used on resetPassword method
-    protected $isTrustedAction = false;
+    public $isTrustedAction = false;
 
     static $_current = null;
 
