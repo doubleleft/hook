deec2ac159e28399540eff345bf3d92e60549068
Endel Dreyer <endel.dreyer@gmail.com>
2015-04-01 16:38:27 -0300

    form block helper now auto-set App-Id/App-Key on query string

--- src/View/BlockHelper.php
+++ src/View/BlockHelper.php
@@ -35,10 +35,23 @@ class BlockHelper {
         $args = func_get_args();
         $options = array_pop($args);
 
+        // use empty string as default action
+        if (!isset($options['hash']['action'])) {
+            $options['hash']['action'] = "";
+        }
+
         // use GET method as default
         if (!isset($options['hash']['method'])) {
             $options['hash']['method'] = 'get';
         }
+
+        // evaluate action url
+        $app_key = \Hook\Application\Context::getKey();
+        $action_url = parse_url($options['hash']['action']);
+        if (!isset($action_url['query'])) {
+            $action_url['query'] = 'X-App-Id=' . $app_key->app_id . '&X-App-Key=' . $app_key->key;
+        }
+        $options['hash']['action'] = unparse_url($action_url);
 
         $html = '<form' . html_attributes($options['hash']) . '>' . "\n" .
             $options['fn']() .
--- src/bootstrap/helpers.php
+++ src/bootstrap/helpers.php
@@ -18,6 +18,19 @@ function public_url($segments, $protocol = null)
     return  $protocol . '://' . $_SERVER['SERVER_NAME'] . $path . $segments;
 }
 
+function unparse_url($parsed_url) {
+    $scheme = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
+    $host = isset($parsed_url['host']) ? $parsed_url['host'] : '';
+    $port = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
+    $user = isset($parsed_url['user']) ? $parsed_url['user'] : '';
+    $pass = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
+    $pass = ($user || $pass) ? "$pass@" : '';
+    $path = isset($parsed_url['path']) ? $parsed_url['path'] : '';
+    $query = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
+    $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
+    return "$scheme$user$pass$host$port$path$query$fragment";
+}
+
 function to_json($data) {
     if (is_string($data)) {
         $json = $data;
