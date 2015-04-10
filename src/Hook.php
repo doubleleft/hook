8db20a32aedec38966c7b987b4d85a845b4fc730
Endel Dreyer <endel.dreyer@gmail.com>
2015-04-10 20:10:12 -0300

    migrating to slim3. work in progress.

--- src/Hook.php
+++ src/Hook.php
@@ -2,18 +2,22 @@
 // global helpers
 require __DIR__ . '/bootstrap/helpers.php';
 
-$config = require(__DIR__ . '/../config/preferences.php');
-date_default_timezone_set($config['timezone']);
+$settings = require(__DIR__ . '/../config/preferences.php');
+date_default_timezone_set($settings['timezone']);
 
-if ($config['debug']) {
+if ($settings['debug']) {
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
 }
 
-$config['log.enabled'] = $config['debug'];
+$settings['log.enabled'] = $settings['debug'];
 
-// Merge settings with security config
-$config = array_merge($config, require(__DIR__ . '/../config/security.php'));
+// Merge settings with security settings
+$settings = array_merge($settings, require(__DIR__ . '/../config/security.php'));
 
-$app = new \Slim\Slim($config);
+$settings['database'] = require(__DIR__ . '/../config/database.php');
+$settings['paths'] = require(__DIR__ . '/../config/paths.php');
+$settings['view'] = new Hook\View\View();
+
+$app = new \Slim\App($settings);
 return $app;
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,15 +1,11 @@
 <?php
 $app = require __DIR__ . '/Hook.php';
 
-$app->config('database', require(__DIR__ . '/../config/database.php'));
-$app->config('paths', require(__DIR__ . '/../config/paths.php'));
-$app->config("view", new Hook\View\View());
-
 Hook\Http\Router::setInstance($app);
 
 require __DIR__ . '/bootstrap/connection.php';
 
-foreach($app->config('aliases') as $alias => $source) {
+foreach($app['settings']['aliases'] as $alias => $source) {
     class_alias($source, $alias);
 }
 
