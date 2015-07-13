baf28057042daad507fc4f5496383d5f7379ce33
Endel Dreyer <endel.dreyer@gmail.com>
2015-07-13 15:52:46 -0300

    set default dateFormat on all Models. closes #147

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -8,4 +8,12 @@ class Model extends \DLModel
 {
     protected $guarded = array();
     protected $primaryKey = '_id';
+
+    //
+    // Setup default date format
+    // Use a string representing an RFC2822 or ISO 8601 date
+    // http://tools.ietf.org/html/rfc2822#page-14
+    //
+    protected $dateFormat = 'Y-m-d\TH:i:sP';
+
 }
